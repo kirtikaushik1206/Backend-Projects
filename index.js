20ce27187ba40478bed079ef6daf84f65490cbdb
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-14 23:21:54 +0530

    Updated

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.set("view engine" , "ejs");
 app.set ("views" , path.resolve("./views"));
 
 
+app.get('/' , (req,res) =>{
+    res.render('home');
+})
+
 app.listen(PORT , ()=>{
     console.log(`server started at PORT : ${PORT} `)
 });
