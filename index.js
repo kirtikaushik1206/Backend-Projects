a754ada88c96aa0577a53fe080d1da63f8aba81f
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-16 02:37:18 +0530

    Updated

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require ('express');
+const mongoose= require("mongoose");
+const userRoute = require('./routes/index')
 const app = express();
 const PORT = 8000;
 
+mongoose.connect(`mongodb://127.0.0.1:27017/blogify`).then(e=> console.log("MongoDb is connected"))
 app.set("view engine" , "ejs");
 app.set ("views" , path.resolve("./views"));
 
@@ -14,3 +17,4 @@ app.get('/' , (req,res) =>{
 app.listen(PORT , ()=>{
     console.log(`server started at PORT : ${PORT} `)
 });
+app.use("/user" , userRoute);
