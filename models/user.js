01bfbe3c9ca05ad57fada1360a434c187f9ac1ae
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-15 22:49:48 +0530

    hashed Password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const {createHmac , randomBytes}= require("crypto");
+
+
 const {Schema , model  } = require ('mongoose')
 
 const userSchema = new Schema( {
@@ -20,6 +23,8 @@ salt:{
 password:{
     type:String,
     required:true,
+  
+
 
 },
 
@@ -37,6 +42,19 @@ role:{
 },
 {timestamps:true}
 );
+ // Mongoose middleware mein hamesha regular function likhte hain kyunki this ke through hi current document ka data milta hai — arrow function mein woh context lost ho jaata hai!
+ userSchema.pre("save" , function(next){
+    if(!user.isModified("password")) return;
+    const salt= randomBytes(16).toString(); //generated salt
+   const hashedPassword= createHmac("sha256" , salt)
+   .update(user.password)
+   .digest("hex");
+
+   this.salt = salt;
+   this.password= hashedPassword;
+
+   next();
+ });
 
 const User = model('user' , userSchema);
 
