0ba833be2931b342025ec8fdd792a628370915af
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-20 21:06:35 +0530

    setting up authentication

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require ('express');
 const mongoose= require("mongoose");
-const userRoute = require('./routes/index')
+const cookieParser= require("cookie-parser");
+const userRoute = require('./routes/index');
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 const app = express();
 const PORT = 8000;
 
@@ -9,6 +11,8 @@ mongoose.connect(`mongodb://127.0.0.1:27017/blogify`).then(e=> console.log("Mong
 app.set("view engine" , "ejs");
 app.set ("views" , path.resolve("./views"));
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
 
 app.get('/' , (req,res) =>{
     res.render('home');
